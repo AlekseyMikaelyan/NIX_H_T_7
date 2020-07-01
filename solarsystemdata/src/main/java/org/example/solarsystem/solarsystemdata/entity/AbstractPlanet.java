9602baf1e572d52ed0401e8c73f8c664a676ffe5
Aleksey Mikaelyan <mikaelaleksey@gmail.com>
2020-07-01 18:28:15 +0300

    Start working. Create modules solarsystemmain, solarsystemdata, solarsystemlogic

---
+++ solarsystemdata/src/main/java/org/example/solarsystem/solarsystemdata/entity/AbstractPlanet.java
@@ -0,0 +1,23 @@
+package org.example.solarsystem.solarsystemdata.entity;
+
+public class AbstractPlanet {
+    private double weight;
+    private double radius;
+    public final double gravitationalConstant = 6.6720 * Math.pow(10.0, -11.0);
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
---
+++ solarsystemmain/src/main/java/org/example/solarsystem/solarsystemmain/Main.java
@@ -0,0 +1,7 @@
+package org.example.solarsystem.solarsystemmain;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
